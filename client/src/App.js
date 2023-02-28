126178425b7346e481e4b9b57af3b84e71912425
Daniel Hervás <danielhervasg@gmail.com>
2023-03-01 00:47:30 +0100

    remove unused boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,6 @@ import React, { Component } from 'react';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './components/Home';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import About from './components/About';
-import Contact from './components/Contact';
-import NotFound from './components/NotFound';
-
 import './App.css';
 
 class App extends Component {
@@ -20,10 +13,6 @@ class App extends Component {
         <main className="flex-shrink-0">
           <div className="container">
             <Routes>
-              <Route exact path="/" element={<Home/>} />
-              <Route path="/about" element={<About/>} />
-              <Route path="/contact" element={<Contact/>} />
-              <Route path="*" element={<NotFound/>} />
             </Routes>
           </div>
         </main>
